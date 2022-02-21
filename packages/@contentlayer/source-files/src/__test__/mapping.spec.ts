c6d2a67fa2044b19d238a00123851ecfa2e19492
Johannes Schickling <schickling.j@gmail.com>
2022-02-21 22:21:52 +0000

    Upgrade vitest. Seems to work now.

--- packages/@contentlayer/source-files/src/__test__/mapping.spec.ts
+++ packages/@contentlayer/source-files/src/__test__/mapping.spec.ts
@@ -1,7 +1,7 @@
-// import * as core from '@contentlayer/core'
-// import { DummyTracing } from '@contentlayer/utils'
-// import type { HasClock, HasConsole, OT } from '@contentlayer/utils/effect'
-// import { pipe, provideConsole, T } from '@contentlayer/utils/effect'
+import * as core from '@contentlayer/core'
+import { DummyTracing } from '@contentlayer/utils'
+import type { HasClock, HasConsole, OT } from '@contentlayer/utils/effect'
+import { pipe, provideConsole, T } from '@contentlayer/utils/effect'
 import { expect, test } from 'vitest'
 
 import { getFlattenedPath, testOnly_getDataForFieldDef as getDataForFieldDef } from '../fetchData/mapping.js'
@@ -15,57 +15,60 @@ test('getFlattenedPath', async () => {
   // t.is(getFlattenedPath('index.md'), '')
 })
 
-// const __unusedValue: any = ''
+const __unusedValue: any = ''
 
-// test('getDataForFieldDef', async (t) => {
-//   const testValue = async ({
-//     type,
-//     expectedValue,
-//     rawFieldData,
-//     options,
-//   }: {
-//     type: 'date'
-//     rawFieldData: any
-//     expectedValue: any
-//     options?: Partial<core.PluginOptions>
-//   }) => {
-//     const transformedData = await runPromise(
-//       getDataForFieldDef({
-//         rawFieldData,
-//         contentDirPath: __unusedValue,
-//         fieldDef: {
-//           type,
-//           name: 'someField',
-//           isSystemField: false,
-//           isRequired: false,
-//           default: undefined,
-//           description: undefined,
-//         },
-//         coreSchemaDef: { hash: '', documentTypeDefMap: {}, nestedTypeDefMap: {} },
-//         relativeFilePath: __unusedValue,
-//         options: {
-//           fieldOptions: core.defaultFieldOptions,
-//           markdown: undefined,
-//           mdx: undefined,
-//           date: undefined,
-//           ...options,
-//         },
-//       }),
-//     )
+test('getDataForFieldDef', async (t) => {
+  const testValue = async ({
+    type,
+    expectedValue,
+    rawFieldData,
+    options,
+  }: {
+    type: 'date'
+    rawFieldData: any
+    expectedValue: any
+    options?: Partial<core.PluginOptions>
+  }) => {
+    const transformedData = await runPromise(
+      getDataForFieldDef({
+        rawFieldData,
+        contentDirPath: __unusedValue,
+        fieldDef: {
+          type,
+          name: 'someField',
+          isSystemField: false,
+          isRequired: false,
+          default: undefined,
+          description: undefined,
+        },
+        coreSchemaDef: { hash: '', documentTypeDefMap: {}, nestedTypeDefMap: {} },
+        relativeFilePath: __unusedValue,
+        options: {
+          fieldOptions: core.defaultFieldOptions,
+          markdown: undefined,
+          mdx: undefined,
+          date: undefined,
+          ...options,
+        },
+      }),
+    )
 
-//     t.is(transformedData, expectedValue)
-//   }
+    expect(transformedData).toBe(expectedValue)
+    // t.is(transformedData, expectedValue)
+  }
 
-//   await testValue({ type: 'date', rawFieldData: '2022', expectedValue: '2022-01-01T00:00:00.000Z' })
-//   await testValue({ type: 'date', rawFieldData: '2022/10/12', expectedValue: '2022-10-12T00:00:00.000Z' })
-//   await testValue({ type: 'date', rawFieldData: '2022-10-12', expectedValue: '2022-10-12T00:00:00.000Z' })
-//   await testValue({
-//     type: 'date',
-//     rawFieldData: '2022-10-12',
-//     expectedValue: '2022-10-12T04:00:00.000Z',
-//     options: { date: { timezone: 'America/New_York' } },
-//   })
-// })
+  await testValue({ type: 'date', rawFieldData: '2022', expectedValue: '2022-01-01T00:00:00.000Z' })
+  await testValue({ type: 'date', rawFieldData: '2022/10/12', expectedValue: '2022-10-12T00:00:00.000Z' })
+  await testValue({ type: 'date', rawFieldData: '2022-10-12', expectedValue: '2022-10-12T00:00:00.000Z' })
+  await testValue({
+    type: 'date',
+    rawFieldData: '2022-10-12',
+    expectedValue: '2022-10-12T04:00:00.000Z',
+    options: { date: { timezone: 'America/New_York' } },
+  })
 
-// const runPromise = (eff: T.Effect<OT.HasTracer & HasClock & HasConsole, unknown, any>) =>
-//   pipe(eff, T.provide(DummyTracing), provideConsole, T.runPromise)
+  t()
+})
+
+const runPromise = (eff: T.Effect<OT.HasTracer & HasClock & HasConsole, unknown, any>) =>
+  pipe(eff, T.provide(DummyTracing), provideConsole, T.runPromise)
--- packages/@contentlayer/source-files/vitest.config.ts
+++ packages/@contentlayer/source-files/vitest.config.ts
@@ -1,9 +1,10 @@
-/// <reference types="vitest" />
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    include: ['dist/__test__/**/mapping.spec.js'],
+    // TODO https://github.com/nuxt/framework/issues/3252
+    // include: ['dist/__test__/**/mapping.spec.js'],
+    include: ['src/__test__/**/mapping.spec.ts'],
     exclude: [],
     watchIgnore: [],
   },
